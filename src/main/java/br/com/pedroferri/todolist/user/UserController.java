3d9360a69c802610d4662e4b20bba547818220a5
Pedro Ferri <pedroferri@edu.unifil.br>
2023-10-10 14:49:53 -0300

    Criacao do banco de dados em memoria Feito a criacaao do banco de dados em memoriaa, aas tabelas e as ligaacoes com o banco de dadcos

--- src/main/java/br/com/pedroferri/todolist/user/UserController.java
+++ src/main/java/br/com/pedroferri/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.pedroferri.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    /*
-     * String (texto)
-     * Integer (numeros inteiros)
-     * Double (Numero com cas decimal )
-     * Float (Casa decimaaal com numero menor que o double)
-     * char (aceita apenas um caracter)
-     * date (retorn data)
-     * void (sem retorno)
-     */
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            System.out.println("Usuario ja existe");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
+        var userCreated =this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
