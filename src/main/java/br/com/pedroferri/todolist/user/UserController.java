a839d00db134813a25e1c7573c21f1828daa12be
Pedro Ferri <pedroferri@edu.unifil.br>
2023-10-11 15:15:46 -0300

    Enviando arquivos da aula 3

--- src/main/java/br/com/pedroferri/todolist/user/UserController.java
+++ src/main/java/br/com/pedroferri/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /*
  * Modificadores
  * public
@@ -20,7 +22,7 @@ public class UserController {
     
     @Autowired
     private IUserRepository userRepository;
-
+    
     @PostMapping("/")
     public ResponseEntity create(@RequestBody UserModel userModel){
         var user = this.userRepository.findByUsername(userModel.getUsername());
@@ -29,6 +31,11 @@ public class UserController {
 
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
         }
+
+        var passwordHashered = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashered);
+
         var userCreated =this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
